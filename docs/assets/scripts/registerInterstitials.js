6e93938d5ecc2fb78ff4260619c6791c9613a5ad
Julian Boilen <julianboilen@gmail.com>
2020-01-12 10:54:26 -0500

    Typo

--- docs/assets/scripts/registerInterstitials.js
+++ docs/assets/scripts/registerInterstitials.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', function () {
   const els = document.querySelectorAll('a[data-use-interstitial]');
   els.forEach(function(el) {
     el.addEventListener('click', function(e) {
-      if (!DEMO && Cookie.get('signed-up')) return true;
+      if (!DEMO && Cookies.get('signed-up')) return true;
       window.showPdfInterstitial(el.getAttribute('href'));
       e.preventDefault();
     });
